04e82f00fb3f4dadfe856fd5801d73602ba374f7
saura <sksingh5087@gmail.com>
2022-03-01 12:04:47 +0530

    Third Commit

---
+++ LearningJava/src/MainBox.java
@@ -0,0 +1,23 @@
+class Box {
+	private int length, breadth, height;
+	public void  setDimension(int l, int b, int h) {
+		length =l; breadth=b; height=h;
+	}
+	public void showDimensions() {
+		System.out.println("L="+length);
+		System.out.println("B="+breadth);
+		System.out.println("H="+height);
+	}	
+}
+
+public class MainBox {
+	public static void main(String[] args) {
+		Box obj1Box = new Box();
+		obj1Box.setDimension(10, 12, 15);
+		obj1Box.showDimensions();
+		
+		obj1Box=new Box();
+		obj1Box.showDimensions();
+
+	}
+}
